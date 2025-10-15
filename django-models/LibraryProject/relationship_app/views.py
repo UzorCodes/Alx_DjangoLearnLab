3da6667b569c668c6d3ed676af382a9b4fb39997
Paul <paulezumezu@gmail.com>
2025-10-15 19:22:48 +0100

    Authentication set up edit 4

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -34,9 +34,9 @@ class LibraryDetailView(DetailView):
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
-    template_name = 'register.html'
+    template_name = 'relationship_app/register.html'
 
 
 
