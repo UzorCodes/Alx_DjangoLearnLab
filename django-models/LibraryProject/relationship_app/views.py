1a3690991adc389d58a8de1f938191714d53f51f
Paul <paulezumezu@gmail.com>
2025-10-15 19:14:29 +0100

    Authentication set up edit 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login, logout
+
 # Create your views here.
 
 
